ad8a51edf7937d6c7d3c7f68c7fdfe8512444fb7
VIAL Sébastien <sebvi66@hotmail.fr>
2024-03-21 09:02:54 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import * as user_types from "./user"
+
+export default {
+  ...user_types
+}
---
+++ src/user.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const LoginZodSchema = z.object({
+  mail: z.string().email(),
+  password: z.string().min(5),
+})
+
+export type LoginType = z.infer<typeof LoginZodSchema>
+
+export const UserZodSchema = z.object({
+  mail: z.string().email({ message: "Adresse e-mail invalide" }),
+  password: z.string().min(6, { message: "Votre mot de passe doit avoir une longueur de 6 caractères" }),
+  first_name: z.string().min(5, { message: "Votre prénom doit faire au moins 2 caractères" }),
+  last_name: z.string().min(2, { message: "Votre nom doit faire au moins 2 caractères" }),
+  phone_number: z.string(),
+});
+
+export type UserType = z.infer<typeof UserZodSchema>;
