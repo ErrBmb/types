f9237f4a35fc651cbc1a9142bae54bfd76d62022
VIAL Sébastien <sebvi66@hotmail.fr>
2024-04-13 16:19:24 +0200

    feat: research and offer types

---
+++ research.ts
@@ -0,0 +1,22 @@
+import { z } from "zod"
+import { LocationZodSchema } from "./location"
+
+export const ResearchZodType = z.object({
+  place: z.string(),
+  checkIn: z.date(),
+  checkOut: z.date(),
+  bedrooms: z
+    .number()
+    .min(0, { message: "Le nombre minimum à indiquer est 0." }),
+  beds: z.number().min(0, { message: "Le nombre minimum à indiquer est 0." }),
+  distance: z.number(),
+})
+export type ResearchType = z.infer<typeof ResearchZodType>
+
+export const OfferZodType = z.object({
+  location: LocationZodSchema,
+  checkIn: z.date(),
+  checkOut: z.date(),
+  totalPrice: z.number(),
+})
+export type OfferType = z.infer<typeof OfferZodType>
